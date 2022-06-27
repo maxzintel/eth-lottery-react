27744a3c3a575554618042c0232bc3df74dda351
s1dequest <maxzintel@gmail.com>
2022-06-27 10:12:48 -0400

    Add call to get balance currently managed by the contract.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // Need to do more react learning to know what this is doing.
+  state = {
+    manager: '',
+    players: [],
+    balance: '' // balance is not technically a number, its an object wrapped in bignumber.js
+  };
 
-    this.state = { manager: '' };
-  }
   async componentDidMount() {
     // we dont need to set 'from: account[0]' here because metamasks provider that we are interacting with here has a default: the first account associated with the wallet.
     const manager = await lottery.methods.manager().call();
-
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address); // used in tests in other repo! In wei by default.
+    
+    this.setState({ manager, players, balance });
   }
 
   render() {
@@ -22,6 +26,7 @@ class App extends React.Component {
       <div className="Lottery">
         <h2>Lottery Contract!</h2>
         <p>This contract is managed by {this.state.manager}</p>
+        <p>There are {this.state.players.length} people entered in the lottery so far, all competing for the grand prize of {web3.utils.fromWei(this.state.balance, 'ether')} eth!</p>
       </div>
     );
   }
